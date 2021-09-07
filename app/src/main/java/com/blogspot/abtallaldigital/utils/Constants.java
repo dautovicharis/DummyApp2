a0ce0c5fc8c98041062b8315adcdcd0d9df0e25e
Mido <Mohammed.Professional@yahoo.com>
2021-09-07 23:44:43 +0200

    added Secrets Gradle Plugin for Android and gitignore rules

--- app/src/main/java/com/blogspot/abtallaldigital/utils/Constants.java
+++ app/src/main/java/com/blogspot/abtallaldigital/utils/Constants.java
@@ -1,7 +1,9 @@
 package com.blogspot.abtallaldigital.utils;
 
+import com.blogspot.abtallaldigital.BuildConfig;
+
 public class Constants {
-    private static final String KEY = "";
+    private static final String KEY = BuildConfig.BLOGGER_KEY;
     private static final String BASE_URL = "https://www.googleapis.com/blogger/v3/blogs/4294497614198718393/posts/";
     private static final String BASE_URL_POSTS_BY_LABEL
             = "https://www.googleapis.com/blogger/v3/blogs/4294497614198718393/";
